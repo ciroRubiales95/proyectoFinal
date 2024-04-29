a31b23004a3bc27a376433d951689437ab9ea829
cirom <cirom.rubiales@gmail.com>
2024-04-29 17:04:27 +0200

    Agcambio en Departamento

--- src/main/Departamento.java
+++ src/main/Departamento.java
@@ -6,16 +6,4 @@ public class Departamento {
         this.id = id;
         this.nombre = nombre;
     }
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
 }
